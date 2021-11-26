2442db0536cbd45842d0d7d170d1e35026bdc695
ethanDe <ethand971@deltalearns.ca>
2021-11-26 14:44:37 -0800

    Done

--- assignment_test.py
+++ assignment_test.py
@@ -1,13 +1,7 @@
 #! python3
 
-import assignment
+import task1
 
 def test1():
-  assert assignment.function() == expected output
-
-def test2():
-  assert "needle" in assignment.function()
-  assert "needle2" in assignment.function(),"error Message"
-
-def test3():
-  assert assignment.function() == expected output
+    assert task1.find('apple') == 0
+    assert task1.find('fish') == 5
